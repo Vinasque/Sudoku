93fc69fdb8b04fd38ef29a09c0dc7b7a7ce2375d
Vinasque <b51986@fgv.edu.br>
2023-11-03 01:02:23 -0300

    Tudo já feito até agora

---
+++ funcoes.py
@@ -0,0 +1,50 @@
+import numpy as np
+import networkx as nx
+
+def vizinhos(num_quad, num_analise):
+    linhas = []
+    for n in range(1, num_quad ** 2, num_quad):
+        linha = list(range(n, n + num_quad))
+        linhas.append(linha)
+    sudoku = np.array(linhas)
+
+    # Encontre a posição (linha e coluna) do número na matriz
+    posicao = np.where(sudoku == num_analise)
+    linha, coluna = posicao[0][0], posicao[1][0]
+
+    linha_vizinhos = list(sudoku[linha, :])
+    coluna_vizinhos = list(sudoku[:, coluna])
+
+    # Encontre os números no mesmo bloco
+    bloco_tamanho = int(sudoku.shape[0] ** 0.5)
+    bloco_linha = linha // bloco_tamanho * bloco_tamanho
+    bloco_coluna = coluna // bloco_tamanho * bloco_tamanho
+    bloco_vizinhos = list(sudoku[bloco_linha:bloco_linha + bloco_tamanho, bloco_coluna:bloco_coluna + bloco_tamanho].flatten())
+
+    vizinhos = set(linha_vizinhos + coluna_vizinhos + bloco_vizinhos) - {num_analise}
+
+    return list(vizinhos)
+
+def dicionario(n_quad):
+    # Crie um grafo
+    G = nx.Graph()
+
+    # Adicione nós
+    for n in range(1, n_quad ** 2 + 1):
+        G.add_node(f"{n}")
+
+    # Adicione arestas
+    for n in range(1, n_quad ** 2 + 1):
+        vizinhanca = vizinhos(n_quad, n)
+        for num in vizinhanca:
+            G.add_edge(f"{n}", f"{num}")
+
+    # Cria o dicionário
+    grafo_dicionario = dict()
+    for vertice in G.nodes():
+        vizinhos_vertice = list(G.neighbors(vertice))
+        grafo_dicionario[vertice] = vizinhos_vertice
+
+    return grafo_dicionario
+
+print(dicionario(4))
---
+++ grafo.py
@@ -0,0 +1,42 @@
+from funcoes import vizinhos
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+def grafo(n_quad, nome):
+    # Crie um grafo
+    G = nx.Graph()
+
+    # Adicione nós
+    for n in range(1, n_quad ** 2 + 1):
+        G.add_node(f"{n}")
+
+    # Adicione arestas
+    for n in range(1, n_quad ** 2 + 1):
+        vizinhanca = vizinhos(n_quad, n)
+        for num in vizinhanca:
+            G.add_edge(f"{n}", f"{num}")
+
+    # Daqui para baixo é só estética
+
+    # Organize os vértices pelo valor do vértice em ordem crescente no sentido horário
+    vertices_crescentes = sorted(G.nodes(), key=lambda x: int(x), reverse=True)
+    angulos = np.linspace(0, 2 * np.pi, len(G.nodes()), endpoint=False)
+    posicao = {vertice: (np.cos(angulos[i]), np.sin(angulos[i])) for i, vertice in enumerate(vertices_crescentes)}
+
+    # Defina as cores dos nós
+    vertices_cores = ['white'] * len(G.nodes())
+    vertices_cores_borda = ['black'] * len(G.nodes())
+
+    # O formato do grafo personalizado
+    plt.figure(figsize=(6, 6))
+    nx.draw(G, posicao, with_labels = True, node_color = vertices_cores, edge_color='black', node_size=500, font_weight='bold')
+    nx.draw_networkx_edges(G, posicao, edgelist = G.edges(), edge_color='black', width=2)  # Desenhe as arestas novamente para que fiquem acima dos nós
+    nx.draw_networkx_nodes(G, posicao, node_color = 'white', node_size = 500, node_shape = 'o', edgecolors = vertices_cores_borda, linewidths = 2)
+
+    plt.axis('off')
+    plt.savefig(nome)
+    plt.show()
+
+# Área de testes
+grafo(4, "4x4.svg")
